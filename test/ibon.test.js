22410a0152188cef9a2cfbaec82ddba15e860283
“buzonliao” <buzonliao@gmail.com>
2020-03-28 13:17:04 +0800

    modify test

--- test/ibon.test.js
+++ test/ibon.test.js
@@ -1,7 +1,6 @@
 const ibon = require('../lib/ibon');
 
-test('Response with expected key - code, storeName, address', () => {
-    return ibon('台北市').then((res) => {
-        expect(res[1]).toHaveProperty('code', 'storeName', 'address');
-    });
+test('Response with expected key - code, storeName, address', async () => {
+    const res = await ibon('台北市');
+    expect(res[1]).toHaveProperty('code', 'storeName', 'address')
 })
