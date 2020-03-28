6065d9227568c5c55a19cac73045cd3db57760e1
“buzonliao” <buzonliao@gmail.com>
2020-03-28 17:33:16 +0800

    add TODO

--- __test__/ibon.test.js
+++ __test__/ibon.test.js
@@ -1,6 +1,7 @@
 const { parseStoreInfo, ibonAPI } = require('../lib/ibon');
 
 test('Response with expected key - code, storeName, address', async () => {
+  //TODO: mock ibonAPI
   const ibInfo = await ibonAPI('台北市');
   const storeInfo = await parseStoreInfo(ibInfo);
   expect(storeInfo[1]).toHaveProperty('code', 'storeName', 'address')
